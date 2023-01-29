d4bd5844096a1a847429fdb234012e02d1d9aa47
gmarseglia <g.marseglia.it@gmail.com>
2023-01-29 18:33:56 +0100

    #84: Tables updated.

--- SQL/Users-Tables-root.sql
+++ SQL/Users-Tables-root.sql
@@ -26,182 +26,145 @@ USE bacheca_annunci;
 -- Create tables
 USE bacheca_annunci;
 
---		Creo utente
 CREATE TABLE utente (
-	username 			VARCHAR(30) 	NOT NULL,
+	username 			VARCHAR(30),
 	annunci_inseriti 	INT UNSIGNED 	NOT NULL 	DEFAULT 0,
 	annunci_venduti 	INT UNSIGNED 	NOT NULL 	DEFAULT 0,
-	contr_seguiti 		TIMESTAMP 					DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (username)
 ) ENGINE = InnoDB;
 
---		Creo credenziali
 CREATE TABLE credenziali (
-	username		VARCHAR(30)					NOT NULL,
+	username		VARCHAR(30),
 	password		CHAR(40)					NOT NULL,
 	ruolo			ENUM('base', 'gestore')		NOT NULL,
 	PRIMARY KEY (username),
-	FOREIGN KEY (username) REFERENCES utente(username)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+	FOREIGN KEY (username) REFERENCES utente(username) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
---		Creo anagrafica
 CREATE TABLE anagrafica (
-	codice_fiscale			CHAR(16)				NOT NULL,
+	username				VARCHAR(30),
+	codice_fiscale			CHAR(16)				NOT NULL	UNIQUE,
 	nome					VARCHAR(100)			NOT NULL,
 	cognome					VARCHAR(100) 			NOT NULL,
-	sesso					ENUM('Uomo', 'Donna')	NOT NULL,
+	sesso					ENUM('uomo', 'donna')	NOT NULL,
 	data_nascita    		DATE					NOT NULL,
 	comune_nascita			VARCHAR(100)			NOT NULL,
-	indirizzo_residenza		VARCHAR(100)			NOT NULL,
-	indirizzo_fatturazione	VARCHAR(100),
-	utente					VARCHAR(30)				NOT NULL,
-	PRIMARY KEY (codice_fiscale),
-	FOREIGN KEY (utente) REFERENCES utente(username)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+	via_residenza			VARCHAR(100)			NOT NULL,
+	civico_residenza		VARCHAR(100)			NOT NULL,
+	cap_residenza			CHAR(5)					NOT NULL,
+	via_fatturazione		VARCHAR(100),
+	civico_fatturazione		VARCHAR(100),
+	cap_fatturazione		CHAR(5),
+	PRIMARY KEY (username),
+	FOREIGN KEY (username) REFERENCES utente(username) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
---		Creo recapito
 CREATE TABLE recapito (
-	valore			VARCHAR(60)									NOT NULL,
-	anagrafica		CHAR(16)									NOT NULL,
-	tipo			ENUM('telefono', 'cellulare', 'email')		NOT NULL,
-	PRIMARY KEY (valore),
-	KEY (valore, anagrafica),
-	FOREIGN KEY (anagrafica) REFERENCES anagrafica(codice_fiscale)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE
+	valore			VARCHAR(60),
+	tipo			ENUM('telefono', 'cellulare', 'email'),
+	anagrafica		VARCHAR(30)									NOT NULL,
+	PRIMARY KEY (valore, tipo),
+	KEY (valore, tipo, anagrafica),
+	FOREIGN KEY (anagrafica) REFERENCES anagrafica(username) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
---		Creo recapito_preferito
 CREATE TABLE recapito_preferito (
-	anagrafica		CHAR(16),
-	recapito		VARCHAR(60) NOT NULL UNIQUE,
+	anagrafica	VARCHAR(30),
+	valore		VARCHAR(60),
+	tipo		ENUM('telefono', 'cellulare', 'email'),
 	PRIMARY KEY (anagrafica),
-	FOREIGN KEY (recapito, anagrafica) REFERENCES recapito(valore, anagrafica)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE
+	FOREIGN KEY (valore, tipo, anagrafica) REFERENCES recapito(valore, tipo, anagrafica) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
---		Creo messaggio_privato
 CREATE TABLE messaggio_privato (
-	mittente		VARCHAR(30)		NOT NULL,
-	destinatario	VARCHAR(30)		NOT NULL,
-	inviato			TIMESTAMP		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+	mittente		VARCHAR(30),
+	destinatario	VARCHAR(30),
+	inviato			TIMESTAMP		DEFAULT CURRENT_TIMESTAMP,
 	testo			VARCHAR(250)	NOT NULL,
-	CONSTRAINT PK_messaggio_privato PRIMARY KEY(mittente, destinatario, inviato),
-	FOREIGN KEY (mittente) REFERENCES utente(username) 
-		ON UPDATE CASCADE
-		ON DELETE CASCADE,
-	FOREIGN KEY (destinatario) REFERENCES utente(username) 
-		ON UPDATE CASCADE
-		ON DELETE CASCADE
+	PRIMARY KEY(mittente, destinatario, inviato),
+	FOREIGN KEY (mittente) REFERENCES utente(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (destinatario) REFERENCES utente(username) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
---		Creo categoria
 CREATE TABLE categoria (
-	nome		VARCHAR(60)		NOT NULL,
+	nome		VARCHAR(60),
 	padre		VARCHAR(60),
 	PRIMARY KEY (nome),
-	FOREIGN KEY (padre) REFERENCES categoria(nome)
-	    ON UPDATE CASCADE
-	    ON DELETE SET NULL
+	FOREIGN KEY (padre) REFERENCES categoria(nome) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE = InnoDB;
 
---		Creo annuncio
 CREATE TABLE annuncio (
-	numero			BIGINT UNSIGNED		NOT NULL	AUTO_INCREMENT,
-	inserzionista	VARCHAR(30)			NOT NULL,
+	numero			INT UNSIGNED		AUTO_INCREMENT,
 	descrizione		TEXT				NOT NULL,
 	categoria		VARCHAR(60)			NOT NULL,
+	inserzionista	VARCHAR(30)			NOT NULL,
 	inserito		TIMESTAMP			NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-	modificato		TIMESTAMP			NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-	venduto			TIMESTAMP,
 	PRIMARY KEY (numero),
-	FOREIGN KEY (inserzionista) REFERENCES utente(username)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE,
-	FOREIGN KEY (categoria) REFERENCES categoria(nome)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+	FOREIGN KEY (inserzionista) REFERENCES utente(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (categoria) REFERENCES categoria(nome) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE = InnoDB;
 
---		Creo segue
+CREATE TABLE annuncio_disponibile (
+	annuncio	INT UNSIGNED,
+	modificato	TIMESTAMP		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (annuncio),
+	FOREIGN KEY	(annuncio) REFERENCES annuncio(numero) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE = InnoDB;
+
+CREATE TABLE annuncio_venduto (
+	annuncio	INT UNSIGNED,
+	venduto		TIMESTAMP		NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (annuncio),
+	FOREIGN KEY	(annuncio) REFERENCES annuncio(numero) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE = InnoDB;
+
 CREATE TABLE segue (
-	utente		VARCHAR(30)			NOT NULL,
-	annuncio	BIGINT UNSIGNED		NOT NULL,
-	CONSTRAINT PK_segue PRIMARY KEY (utente, annuncio),
-	FOREIGN KEY (utente) REFERENCES utente(username)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
-	FOREIGN KEY (annuncio) REFERENCES annuncio(numero)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE
+	utente		VARCHAR(30),
+	annuncio	INT UNSIGNED,
+	controllato	TIMESTAMP		DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (utente, annuncio),
+	FOREIGN KEY (utente) REFERENCES utente(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (annuncio) REFERENCES annuncio(numero) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
 
---		Creo commento
 CREATE TABLE commento (
-	utente			VARCHAR(30)			NOT NULL,
-	annuncio		BIGINT UNSIGNED		NOT NULL,
-	scritto			TIMESTAMP			NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+	utente			VARCHAR(30),
+	annuncio		INT UNSIGNED,
+	scritto			TIMESTAMP			DEFAULT CURRENT_TIMESTAMP,
 	testo			VARCHAR(250)		NOT NULL,
-	CONSTRAINT PK_commento PRIMARY KEY (utente, annuncio, scritto),
-	FOREIGN KEY (utente) REFERENCES utente(username)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE,
-	FOREIGN KEY (annuncio) REFERENCES annuncio(numero)
-	    ON UPDATE CASCADE
-	    ON DELETE CASCADE
+	PRIMARY KEY (utente, annuncio, scritto),
+	FOREIGN KEY (utente) REFERENCES utente(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY (annuncio) REFERENCES annuncio(numero) ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE = InnoDB;
-
-
-
--- RECREATE INDEXES ------------------------------------------------------------------------------------
-
--- Aggiungo gli indici
-CREATE INDEX By_categoria
-	ON annuncio(categoria);
-
-CREATE INDEX By_inserzionista
-	ON annuncio(inserzionista);
-
-CREATE FULLTEXT INDEX By_descrizione
-	ON annuncio(descrizione);
-
-CREATE UNIQUE INDEX By_utente
-	ON anagrafica(utente);
-
-CREATE INDEX By_anagrafica
-	ON recapito(anagrafica);
 
 
 -- GRANT PRIVILEGES -----------------------------------------------------------------------------------
 
 -- Grant to base
-GRANT INSERT, SELECT ON `messaggio_privato` TO 'base';
 GRANT UPDATE, SELECT ON `utente` TO 'base';
 GRANT SELECT ON `anagrafica` TO 'base';
 GRANT SELECT ON `recapito` TO 'base';
 GRANT SELECT ON `recapito_preferito` TO 'base';
-GRANT INSERT, SELECT ON `commento` TO 'base';
-GRANT INSERT, SELECT, DELETE ON `commento` TO 'base';
-GRANT INSERT, UPDATE, SELECT ON `annuncio` TO 'base';
+GRANT INSERT, SELECT ON `messaggio_privato` TO 'base';
 GRANT SELECT ON `categoria` TO 'base';
+GRANT INSERT, UPDATE, SELECT ON `annuncio` TO 'base';
+GRANT INSERT, UPDATE, SELECT, DELETE ON `annuncio_disponibile` TO 'base';
+GRANT INSERT, UPDATE, SELECT ON `annuncio_venduto` TO 'base';
+GRANT INSERT, UPDATE, SELECT, DELETE ON `segue` TO `base`;
+GRANT INSERT, SELECT ON `commento` TO 'base';
 
 -- Grant to gestore
-GRANT INSERT, SELECT ON `messaggio_privato` TO 'gestore';
-GRANT UPDATE, SELECT ON `utente` TO 'gestore';
-GRANT SELECT ON `anagrafica` TO 'gestore';
-GRANT SELECT ON `recapito` TO 'gestore';
-GRANT SELECT ON `recapito_preferito` TO 'gestore';
-GRANT INSERT, SELECT ON `commento` TO 'gestore';
-GRANT INSERT, SELECT, DELETE ON `commento` TO 'gestore';
-GRANT INSERT, UPDATE, SELECT ON `annuncio` TO 'gestore';
-GRANT SELECT ON `categoria` TO 'gestore';
-GRANT SELECT ON `utente` TO 'gestore';
-GRANT SELECT, INSERT ON `categoria` TO 'gestore';
+GRANT UPDATE, SELECT ON `utente` TO 'base';
+GRANT SELECT ON `anagrafica` TO 'base';
+GRANT SELECT ON `recapito` TO 'base';
+GRANT SELECT ON `recapito_preferito` TO 'base';
+GRANT INSERT, SELECT ON `messaggio_privato` TO 'base';
+GRANT INSERT, SELECT ON `categoria` TO 'base';
+GRANT INSERT, UPDATE, SELECT ON `annuncio` TO 'base';
+GRANT INSERT, UPDATE, SELECT, DELETE ON `annuncio_disponibile` TO 'base';
+GRANT INSERT, UPDATE, SELECT ON `annuncio_venduto` TO 'base';
+GRANT INSERT, UPDATE, SELECT, DELETE ON `segue` TO `base`;
+GRANT INSERT, SELECT ON `commento` TO 'base';
 
 -- Grant to registratore
 GRANT INSERT ON `utente` TO 'registratore';
@@ -213,3 +176,21 @@ GRANT INSERT ON `recapito_preferito` TO 'registratore';
 
 -- Show result
 SHOW TABLES;
+
+
+-- RECREATE INDEXES ------------------------------------------------------------------------------------
+
+-- CREATE INDEX By_categoria
+-- 	ON `annuncio_disponibile(categoria)`;
+
+-- CREATE INDEX By_inserzionista
+-- 	ON `annuncio_disponibile(inserzionista)`;
+
+-- CREATE FULLTEXT INDEX By_descrizione
+-- 	ON `annuncio_disponibile(descrizione)`;
+
+-- CREATE UNIQUE INDEX By_utente
+-- 	ON `anagrafica(utente)`;
+
+-- CREATE INDEX By_anagrafica
+-- 	ON `recapito(anagrafica)`;
