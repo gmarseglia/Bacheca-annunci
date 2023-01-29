1760745237d9debbef1f1c567ec96d2b9dac91fb
gmarseglia <g.marseglia.it@gmail.com>
2023-01-29 19:50:05 +0100

    #86: Populate-root updated.

--- SQL/Populate-root.sql
+++ SQL/Populate-root.sql
@@ -7,9 +7,8 @@ DELETE FROM `utente`;
 DELETE FROM `credenziali`;
 DELETE FROM `anagrafica`;
 
-INSERT INTO `utente` (`username`, `annunci_inseriti`, `annunci_venduti`, `contr_seguiti`) VALUES 
-	('user1', 2, 1, '2023-01-05 12:00:00');
 INSERT INTO `utente` (`username`, `annunci_inseriti`, `annunci_venduti`) VALUES 
+	('user1', 2, 1),
 	('user2', 2, 1),
 	('userg', 1, 0),
 	('GioAma', 2, 0),
@@ -22,13 +21,13 @@ INSERT INTO `credenziali` (`username`, `password`, `ruolo`) VALUES
 	('GioAma',	SHA1('gvnn'),		'base'),
 	('MarBia',	SHA1('mr'),		'gestore'),
 	('MasFab',	SHA1('mssm'),		'base');
-INSERT INTO `anagrafica` (`codice_fiscale`, `nome`, `cognome`, `sesso`, `data_nascita`, `comune_nascita`, `indirizzo_residenza`, `indirizzo_fatturazione`, `utente`) VALUES
-	('GNNGNC00A00A123D', 'Giancarlo', 'Giannini', 'uomo', '2000-01-01', 'Firenze', 'Via Bologna, 4', 'Viale Napoli, 15', 'user1'),
-	('LZZNNA00A00A123D', 'Anna', 'Lazzari', 'donna','2001-02-02', 'Bologna', 'Piazza Venezia, 5', 'Piazza Firenze', 'user2'),
-	('MRNVLN00A00A123D', 'Valentina', 'Mariani', 'donna', '1985-06-16', 'Venezia', 'Viale Genova, 1', NULL, 'userg'),
-	('MTAGVN00A00A123D', 'Giovanni', 'Amato', 'uomo', '1960-01-01', 'Roma', 'Via Milano, 1', 'Via Trieste, 10', 'GioAma'),
-	('BNCMRA00A00A123D', 'Maria', 'Bianchi', 'donna', '1965-02-04', 'Milano', 'Piazza Napoli, 2', 'Via Roma, 3', 'MarBia'),
-	('FBRMSS00A00A123D', 'Massimo', 'Fabri', 'uomo', '1970-03-07', 'Napoli', 'Viale Firenze, 3', NULL, 'MasFab');
+INSERT INTO `anagrafica` (`username`, `codice_fiscale`, `nome`, `cognome`, `sesso`, `data_nascita`, `comune_nascita`, `via_residenza`, `civico_residenza`, `cap_residenza`, `via_fatturazione`, `civico_fatturazione`, `cap_fatturazione`) VALUES
+	('user1', 'GNNGNC00A00A123D', 'Giancarlo', 'Giannini', 'uomo', '2000-01-01', 'Firenze', 'Via Bologna', '4', '00000', 'Viale Napoli', '15', '00001'),
+	('user2', 'LZZNNA00A00A123D', 'Anna', 'Lazzari', 'donna','2001-02-02', 'Bologna', 'Piazza Venezia', '5', '00002', 'Piazza Firenze', '1', '00002'),
+	('userg', 'MRNVLN00A00A123D', 'Valentina', 'Mariani', 'donna', '1985-06-16', 'Venezia', 'Viale Genova', '1', '00003', NULL, NULL, NULL),
+	('GioAma', 'MTAGVN00A00A123D', 'Giovanni', 'Amato', 'uomo', '1960-01-01', 'Roma', 'Via Milano', '1', '00004', 'Via Trieste', '10', '00004'),
+	('MarBia', 'BNCMRA00A00A123D', 'Maria', 'Bianchi', 'donna', '1965-02-04', 'Milano', 'Piazza Napoli', '2', '00005', 'Via Roma', '3', '00005'),
+	('MasFab', 'FBRMSS00A00A123D', 'Massimo', 'Fabri', 'uomo', '1970-03-07', 'Napoli', 'Viale Firenze', '3', '00006', NULL, NULL, NULL);
 
 
 -- (RE)INSERIMENTO DEI RECAPITI E DEI RECAPITI PREFERITI
@@ -36,25 +35,25 @@ DELETE FROM `recapito`;
 DELETE FROM `recapito_preferito`;
 
 INSERT INTO `recapito` (`valore`, `anagrafica`, `tipo`) VALUES
-	('3471122334', 'GNNGNC00A00A123D', 'cellulare'),
-	('05112233', 'GNNGNC00A00A123D', 'telefono'),
-	('prova@email.com', 'GNNGNC00A00A123D', 'email'),
-	('331598647', 'GNNGNC00A00A123D', 'cellulare'),
-	('061122334', 'LZZNNA00A00A123D', 'telefono'),
-	('04112568', 'LZZNNA00A00A123D', 'telefono'),
-	('extra@mail.com', 'LZZNNA00A00A123D', 'email'),
-	('b@mail.com', 'MRNVLN00A00A123D', 'email'),
-	('3391234567', 'MTAGVN00A00A123D', 'cellulare'),
-	('061234564', 'BNCMRA00A00A123D', 'telefono'),
-	('a@mail.com', 'FBRMSS00A00A123D', 'email'),
-	('finale@mail.com', 'FBRMSS00A00A123D', 'email');
-INSERT INTO `recapito_preferito` (`anagrafica`, `recapito`) VALUES
-	('GNNGNC00A00A123D', '3471122334'),
-	('LZZNNA00A00A123D', '061122334'),
-	('MRNVLN00A00A123D', 'b@mail.com'),
-	('MTAGVN00A00A123D', '3391234567'),
-	('BNCMRA00A00A123D', '061234564'),
-	('FBRMSS00A00A123D', 'a@mail.com');
+	('3471122334', 'user1', 'cellulare'),
+	('05112233', 'user1', 'telefono'),
+	('prova@email.com', 'user1', 'email'),
+	('331598647', 'user1', 'cellulare'),
+	('061122334', 'user2', 'telefono'),
+	('04112568', 'user2', 'telefono'),
+	('extra@mail.com', 'user2', 'email'),
+	('b@mail.com', 'userg', 'email'),
+	('3391234567', 'GioAma', 'cellulare'),
+	('061234564', 'MarBia', 'telefono'),
+	('a@mail.com', 'MasFab', 'email'),
+	('finale@mail.com', 'MasFab', 'email');
+INSERT INTO `recapito_preferito` (`anagrafica`, `valore`, `tipo`) VALUES
+	('user1', '3471122334', 'cellulare'),
+	('user2', '061122334', 'telefono'),
+	('userg', 'b@mail.com', 'email'),
+	('GioAma', '3391234567', 'cellulare'),
+	('MarBia', '061234564', 'telefono'),
+	('MasFab', 'a@mail.com', 'email');
 
 
 -- (RE)INSERIMENTO DEI MESSAGGI PRIVATI
@@ -85,14 +84,25 @@ INSERT INTO `categoria` (`nome`, `padre`) VALUES
 DELETE FROM `annuncio`;
 ALTER TABLE `annuncio` AUTO_INCREMENT = 1;
 
-INSERT INTO `annuncio` (`inserzionista`, `descrizione`, `categoria`, `inserito`, `modificato`) VALUES
-	('user1', 'Abito lungo verde.', 'Indumenti', '2023-01-01 12:00:00', '2023-01-06 22:00:00'),
-	('user1', 'Jeans.', 'Pantaloni', '2023-01-01 12:10:00', '2023-01-01 12:10:00'),
-	('user2', 'Cargo corti verdi.', 'Pantaloncini', '2023-01-01 09:00:00', '2023-01-07 12:00:00'),
-	('user2', 'Cargo corti blu.', 'Pantaloncini', '2023-01-01 09:30:00', '2023-01-10 12:00:00'),
-	('userg', 'Tshirt rossa.', 'Magliette', '2023-01-07 11:10:00', '2023-01-07 11:10:00'),
-	('GioAma', 'Gazebo blu.', 'Articoli da esterno', '2023-01-07 11:10:00', '2023-01-07 11:10:00'),
-	('GioAma', 'Paletta carina.', 'Giardinaggio', '2023-01-07 11:10:00', '2023-01-07 11:10:00');
+INSERT INTO `annuncio` (`inserzionista`, `descrizione`, `categoria`, `inserito`) VALUES
+	('user1', 'Abito lungo verde.', 'Indumenti', '2023-01-01 12:00:00'),
+	('user1', 'Jeans.', 'Pantaloni', '2023-01-01 12:10:00'),
+	('user2', 'Cargo corti verdi.', 'Pantaloncini', '2023-01-01 09:00:00'),
+	('user2', 'Cargo corti blu.', 'Pantaloncini', '2023-01-01 09:30:00'),
+	('userg', 'Tshirt rossa.', 'Magliette', '2023-01-07 11:10:00'),
+	('GioAma', 'Gazebo blu.', 'Articoli da esterno', '2023-01-07 11:10:00'),
+	('GioAma', 'Paletta carina.', 'Giardinaggio', '2023-01-07 11:10:00');
+
+INSERT INTO `annuncio_disponibile` (`annuncio`, `modificato`) VALUES
+	(2, NULL),
+	(3, '2023-01-07 12:00:00'),
+	(5, NULL),
+	(6, NULL),
+	(7, NULL);
+
+INSERT INTO `annuncio_venduto` (`annuncio`, `venduto`) VALUES
+	(1, '2023-01-06 22:00:00'),
+	(4, '2023-01-10 12:00:00');
 
 
 -- (RE)INSERIMENTO DEI COMMENTI
@@ -107,11 +117,6 @@ INSERT INTO `commento` (`utente`, `annuncio`, `scritto`,  `testo`) VALUES
 	('user1', 3,  '2023-01-02 11:05:00', 'Molto carini.'),
 	('user1', 4,  '2023-01-02 11:10:00', 'Molto carini anche questi.'),
 	('GioAma', 3, '2023-01-07 12:00:00', 'Non sembrano di buona qualità.');
-
-
--- (RI)VENDITA DI ANNUNCI
-UPDATE `annuncio` SET `venduto` = '2023-01-06 22:00:00' WHERE `numero` = 1;
-UPDATE `annuncio` SET `venduto` = '2023-01-10 12:00:00' WHERE `numero` = 4;
 
 -- INSERIMENTO DEI SEGUE
 DELETE FROM `segue`;
