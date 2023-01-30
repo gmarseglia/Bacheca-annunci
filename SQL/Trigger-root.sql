2d4cd1b54fa6d74ad029c26adc0f7697ab9a194c
gmarseglia <g.marseglia.it@gmail.com>
2023-01-30 18:02:40 +0100

    Trigger-root.sql updated.

--- SQL/Trigger-root.sql
+++ SQL/Trigger-root.sql
@@ -1,16 +1,6 @@
 use `bacheca_annunci`;
 
-
 DELIMITER !
-
-DROP TRIGGER IF EXISTS `before_annuncio_update`;
-CREATE TRIGGER `before_annuncio_update`
-BEFORE UPDATE ON `annuncio` FOR EACH ROW
-BEGIN
-    IF (OLD.`venduto` IS NULL AND NEW.`venduto` IS NOT NULL) THEN
-        SET NEW.`modificato` = NEW.`venduto`;
-    END IF;
-END!
 
 DROP TRIGGER IF EXISTS `before_messaggio_privato_insert`;
 CREATE TRIGGER `before_messaggio_privato_insert`
@@ -21,27 +11,9 @@ BEGIN
     END IF;
 END!
 
-DROP TRIGGER IF EXISTS `before_messaggio_privato_update`;
-CREATE TRIGGER `before_messaggio_privato_update`
-BEFORE UPDATE ON `messaggio_privato` FOR EACH ROW
-BEGIN
-    IF (NEW.`mittente`=NEW.`destinatario`) THEN
-        SIGNAL SQLSTATE "45009" SET message_text="Messaggio non valido";
-    END IF;
-END!
-
 DROP TRIGGER IF EXISTS `before_categoria_insert`;
 CREATE TRIGGER `before_categoria_insert`
 BEFORE INSERT ON `categoria` FOR EACH ROW
-BEGIN
-    IF (NEW.`nome`=NEW.`padre`) THEN
-        SIGNAL SQLSTATE "45010" SET message_text="Categoria non valida";
-    END IF;
-END!
-
-DROP TRIGGER IF EXISTS `before_categoria_update`;
-CREATE TRIGGER `before_categoria_update`
-BEFORE UPDATE ON `categoria` FOR EACH ROW
 BEGIN
     IF (NEW.`nome`=NEW.`padre`) THEN
         SIGNAL SQLSTATE "45010" SET message_text="Categoria non valida";
